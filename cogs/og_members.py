dda4e8e998e7258d4c8ae13146d342ea3252ebb3
Tekboxs <tekboxs@gmail.com>
2024-01-28 13:32:28 -0400

    fix async

--- cogs/og_members.py
+++ cogs/og_members.py
@@ -1,46 +1,41 @@
+import asyncio
 import discord
 from discord.ext import commands
 from discord.ext.commands import Context
 from datetime import datetime
+from discord import app_commands
 
-class MemberSinceCommand(commands.Cog, name="MemberSince"):
-    def __init__(self, bot) -> None:
+
+class MemberSinceCog(commands.Cog, name="MemberSince"):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @commands.command(name="addogmember", help="Atribui o cargo 'OG MEMBER' a membros que estão no servidor desde 31/08/2023 ou antes.")
+    @commands.hybrid_command(
+        name="addogmember",
+        description="Atribui o cargo 'OG MEMBER' a membros que estão no servidor desde 31/08/2023 ou antes."
+    )
     @commands.has_role('Manager')
-    async def addogmember(self, ctx: Context):
+    @app_commands.describe(dia="Dia de corte", mes="Mes de corte", ano="Ano de corte")
+    async def addogmember(self, context: Context, dia: int, mes: int, ano: int):
         """
         Verifica todos os membros do servidor e atribui o cargo 'OG MEMBER' àqueles que estão no servidor desde 31/08/2023 ou antes.
-
-        :param ctx: O contexto do comando.
         """
 
-        specified_date = datetime(2023, 8, 31)
-        role = discord.utils.get(ctx.guild.roles, name="OG MEMBER")
+        specified_date = datetime(ano, mes, dia)
+        role = discord.utils.get(context.guild.roles, name="OG MEMBER")
 
         if role is None:
-            await ctx.send("Cargo 'OG MEMBER' não encontrado no servidor.")
-            return
+            role = await context.guild.create_role(name="OG MEMBER", color=discord.Colour.from_str("#A84300"))
 
-        # Paginação: processar membros em grupos de 200
-        members = ctx.guild.members
-        page_size = 200
+        await context.send("Cargo 'OG MEMBER' sendo atribuído aos membros elegíveis.", ephemeral=True)
+        
+        async for member in context.guild.fetch_members(limit=None):
+            if member.joined_at is not None and member.joined_at.date() <= specified_date.date():
+                asyncio.create_task(member.add_roles(role))
+                await asyncio.sleep(0.1)
+        else:
+            await context.send("Verficação concluida.", ephemeral=True)
 
-        for i in range(0, len(members), page_size):
-            # Pegando uma "página" de membros
-            page_members = members[i:i + page_size]
-
-            for member in page_members:
-                if member.joined_at is not None and member.joined_at.date() <= specified_date.date():
-                    await member.add_roles(role)
-
-            # Enviando feedback para o canal após processar cada página
-            await ctx.send(f"Processando membros {i+1} até {min(i + page_size, len(members))}...")
-
-        await ctx.send("Cargo 'OG MEMBER' atribuído aos membros elegíveis.")
 
 async def setup(bot) -> None:
-    await bot.add_cog(MemberSinceCommand(bot))
-
-
+    await bot.add_cog(MemberSinceCog(bot))
