13baf174673ef4808bbe7bb2dd41ecccc89bdcb1
Dviih <dviih2s@gmail.com>
2024-02-22 20:48:16 -0300

    dropdown remove handler (this will need fix)

--- cogs/roles.py
+++ cogs/roles.py
@@ -90,6 +90,24 @@ class Roles(commands.Cog, name="Roles"):
         await interaction.user.add_roles(*roles)
         return await interaction.response.send_message(f"Cargo {[role.name for role in roles]} atribuído com sucesso!", ephemeral=True)
 
+    async def on_dropdown_remove(self, interaction: Interaction) -> None:
+        roles = []
+
+        for value in interaction.data["values"]:
+            role = self.roles[value]
+
+            if role is None:
+                return await interaction.response.send_message("role was not found, ask an administrator to resync "
+                                                               "the guild!", ephemeral=True)
+            roles.append(role)
+
+            if role not in interaction.user.roles:
+                return await interaction.response.send_message(f"you do not have the role {role.name} to be removed!",
+                                                               ephemeral=True)
+
+        await interaction.user.remove_roles(*roles)
+        return await interaction.response.send_message(f"Roles {[role.name for role in roles]} removidos com sucesso!")
+
 
     @commands.hybrid_command(name="choose_role", description="Criar menu de cargos.")
     @commands.has_role('Manager')
