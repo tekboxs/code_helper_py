0d8c6f8ca03b2057348e476516134ee503325ce3
Dviih <dviih2s@gmail.com>
2024-02-22 20:47:34 -0300

    Get rid of some old stuff.

--- cogs/roles.py
+++ cogs/roles.py
@@ -73,8 +73,6 @@ class Roles(commands.Cog, name="Roles"):
                     reason="role for role was not found"
                 )
 
-def get_roles_options() -> list[SelectOption]:
-    role_config = get_roles_list()
     async def on_dropdown_select(self, interaction: Interaction) -> None:
         roles = []
 
@@ -89,57 +87,7 @@ def get_roles_options() -> list[SelectOption]:
             if role in interaction.user.roles:
                 return await interaction.response.send_message("Você já possui esse cargo!", ephemeral=True)
 
-class ProgrammingRoles(commands.Cog, name="ProgrammingRoles"):
-    async def on_dropdown_select(self, interaction: Interaction) -> None:
-        guild = interaction.guild
-        role_name = f"{interaction.data['values'][0]} Dev"
-        role = discord.utils.get(guild.roles, name=role_name)
 
-        if not role:
-            try:
-                role = await guild.create_role(name=role_name)
-
-            except discord.Forbidden:
-                await interaction.response.send_message("Não tenho permissão para criar cargos!", ephemeral=True)
-                return
-
-        member = interaction.user
-
-        if role not in member.roles:
-            await member.add_roles(role)
-            await interaction.response.send_message(f"Cargo {role_name} atribuído com sucesso!", ephemeral=True)
-        else:
-            await interaction.response.send_message("Você já possui esse cargo!", ephemeral=True)
-
-    async def on_remove_button(self, interaction: discord.Interaction):
-        roles_list = get_roles_list()
-        member = interaction.user
-
-        if roles_list and member:
-            roles_to_remove = [f"{role['label']} Dev" for role in roles_list]
-            roles_to_remove = [discord.utils.get(
-                interaction.guild.roles, name=role_name) for role_name in roles_to_remove]
-            roles_to_remove = [
-                role for role in roles_to_remove if role and role in member.roles]
-
-            if roles_to_remove:
-                await member.remove_roles(*roles_to_remove)
-                await interaction.response.send_message("Cargos removidos com sucesso!", ephemeral=True)
-            else:
-                await interaction.response.send_message("Você não possui nenhum dos cargos a serem removidos.",
-                                                        ephemeral=True)
-        else:
-            await interaction.response.send_message("Erro ao processar a ação.", ephemeral=True)
-
-    def setup_view(self) -> discord.ui.View:
-        role_view = MealDropdownView(
-            custom_id_prefix='remove_role_btn',
-            options=get_roles_options(),
-            callback=self.on_dropdown_select
-        )
-
-        role_view.add_item(MealButtonView(callback=self.on_remove_button, label='Remover'))
-        return role_view
 
     @commands.hybrid_command(name="choose_role", description="Criar menu de cargos.")
     @commands.has_role('Manager')
